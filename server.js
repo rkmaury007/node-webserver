ceb038ca16968c8ef9d06aaa79f1b6569398fb7c
rkmaury007 <rkmaurya2710@gmail.com>
2018-09-16 12:15:00 +0530

    Initial commit

---
+++ server.js
@@ -0,0 +1,61 @@
+const express = require('express');
+const hbs = require('hbs');
+const fs = require('fs');
+
+var app = express();
+app.set('view engine', 'hbs');
+const port = process.env.PORT || 3000;
+
+hbs.registerPartials(__dirname + '/views/partials');
+
+app.use((req, res, next) => {
+  var now = new Date().toString();
+  var log = `${now}: ${req.method} ${req.url}`;
+
+  console.log(log);
+  fs.appendFile('server.log', log, (err) => {
+    if(err){
+    console.log('Unable to connect to server');
+  }
+  });
+  next();
+});
+
+// app.use((req, res, next) => {
+//   res.render('maintenance.hbs');
+// });
+
+app.use(express.static(__dirname + '/public'));
+
+hbs.registerHelper('getCurrentYear', () => {
+  return new Date().getFullYear()
+});
+
+hbs.registerHelper('SayLoud', (text) =>{
+  return text.toUpperCase();
+});
+
+app.get('/', (req, res) => {
+  //res.send('<h1>Hi this response</h1>');
+  res.render('home.hbs', {
+    aboutPage:'This is home page',
+    //currentYear:new Date().getFullYear()
+  });
+});
+
+app.get('/about',(req, res) => {
+  res.render('about.hbs',{
+    aboutPage:'This is demo',
+    //currentYear:new Date().getFullYear()
+  });
+});
+
+app.get('/error',(req, res) => {
+  res.send({
+    errorMessage:'Error has occured'
+  });
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on ${port} port`);
+});
